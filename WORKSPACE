98df8272b8d8bfea57719870974789e0cf06cb6c
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2020-03-16 13:16:17 +0100

    Add units library

--- WORKSPACE
+++ WORKSPACE
@@ -15,3 +15,10 @@ http_archive(
   strip_prefix = "googletest-8b6d3f9c4a774bef3081195d422993323b6bb2e0",
   sha256 = "d21ba93d7f193a9a0ab80b96e8890d520b25704a6fac976fe9da81fffb3392e3",
 )
+
+http_archive(
+  name = "com_nholthaus_units",
+  urls = ["https://github.com/nholthaus/units/archive/v2.3.1.zip"],  # 2019-03-14
+  strip_prefix = "units-2.3.1",
+  build_file = "@//:third_party/units/units.BUILD",
+)
--- hello_app/main.cpp
+++ hello_app/main.cpp
@@ -2,18 +2,35 @@
 #include "absl/flags/parse.h"
 #include "absl/strings/str_format.h"
 
+#include "nholthaus/units.h"
+
 #include <iostream>
 #include <vector>
+
+using namespace units;
+using namespace units::literals;
+
+using namespace units::velocity;
+using namespace units::length;
+using namespace units::time;
 
 ABSL_FLAG(std::string, message, "Hello", "A message to tell to recepients");
 ABSL_FLAG(std::vector<std::string>, recepients,
           std::vector<std::string>({"World"}),
           "Comma-separated list of recepients of the message");
 
-int main(int argc, char* argv[]) {
+ABSL_FLAG(double, meters, 10.0, "Distance in meters.");
+ABSL_FLAG(double, seconds, 10.0, "Time in seconds.");
+
+int main(int argc, char *argv[]) {
   absl::ParseCommandLine(argc, argv);
-  for (const auto& recepient : FLAGS_recepients.Get()) {
+  for (const auto &recepient : FLAGS_recepients.Get()) {
     absl::PrintF("%s %s!\n", FLAGS_message.Get(), recepient);
   }
+
+  meters_per_second_t speed =
+      meter_t{FLAGS_meters.Get()} / second_t{FLAGS_seconds.Get()};
+  absl::PrintF("Speed is: %f m/s or %f km/h\n", speed.value(),
+               kilometers_per_hour_t{speed}.value());
   return 0;
 }
